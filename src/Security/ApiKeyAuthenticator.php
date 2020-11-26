cf5667922a3cb4ea14da76871da23a8bbd96f3dc
Anton <aesidorenko@yandex.ru>
2020-11-26 22:04:45 +0300

    updated auth procedures

--- src/Security/ApiKeyAuthenticator.php
+++ src/Security/ApiKeyAuthenticator.php
@@ -4,9 +4,9 @@ namespace App\Security;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -60,17 +60,12 @@ class ApiKeyAuthenticator extends AbstractGuardAuthenticator
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        throw new UnauthorizedHttpException('Basic realm="Access to the staging API"', strtr($exception->getMessageKey(), $exception->getMessageData()));
+        throw new AccessDeniedHttpException('Authentication failed');
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        $data = [
-            // you might translate this message
-            'message' => 'Authentication Required'
-        ];
-
-        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+        throw new UnauthorizedHttpException('Basic realm="Access to the staging API"', 'Authentication Required');
     }
 
     public function getCredentials(Request $request)
